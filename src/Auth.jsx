4cca4a189209f4f1dd25a3cab3dcc044fc6b1f8c
Danial Shafaei <56474459+dadashvespek@users.noreply.github.com>
2023-07-21 13:18:20 +0300

    aaa

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,9 +1,99 @@
 import { useState } from "react";
 import { supabase } from "./supabaseClient";
+import styled, { keyframes } from 'styled-components';
+import { useSpring, animated } from 'react-spring';
+
+const pulse = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(1.05); }
+  100% { transform: scale(1); }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #1F2937; /* changed to a dark navy blue */
+  color: #F9FAFB; /* changed to the previous background color for contrast */
+`;
+
+const Form = styled.form`
+  background-color: #F9FAFB;
+  padding: 2.5rem;
+  border: 1px solid #D1D5DB;
+  border-radius: 15px;
+  width: 100%;
+  max-width: 450px;
+  display: grid;
+  grid-gap: 2rem;
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  color: #1F2937;
+  font-weight: 600;
+  font-size: 2rem;
+  letter-spacing: -1px;
+  margin-bottom: 1rem;
+`;
+
+const Description = styled.p`
+  text-align: center;
+  color: #4B5563;
+  margin-bottom: 2rem;
+  font-size: 1.1rem;
+  line-height: 1.5;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid #D1D5DB;
+  padding: 0.75rem;
+  width: 100%;
+  box-sizing: border-box;
+  transition: border-bottom-color 0.3s ease-in-out;
+  &:focus {
+    border-bottom-color: #3B82F6;
+    outline-color: #3B82F6;
+  }
+  &::placeholder {
+    color: #9CA3AF;
+  }
+  background-color: #F9FAFB;
+  color: #111827;
+`;
+
+const Button = styled.button`
+  padding: 0.75rem 1.25rem;
+  border: none;
+  border-radius: 15px;
+  color: #F9FAFB;
+  background-color: #3B82F6;
+  cursor: pointer;
+  width: 100%;
+  font-size: 1.2rem;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #2563EB;
+    animation: 2s ${pulse} infinite;
+  }
+  &:disabled {
+    background-color: #D1D5DB;
+  }
+`;
 
 export default function Auth() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
+
+  const animation = useSpring({
+    from: { opacity: 0, transform: 'translate3d(0,40px,0)' },
+    to: { opacity: 1, transform: 'translate3d(0,0px,0)' },
+    delay: 300,
+    config: { mass: 1, tension: 280, friction: 60 },
+  });
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -20,16 +110,15 @@ export default function Auth() {
   };
 
   return (
-    <div className="row flex flex-center">
-      <div className="col-6 form-widget">
-        <h1 className="header">Supabase + React</h1>
-        <p className="description">
-          Sign in via magic link with your email below
-        </p>
-        <form className="form-widget" onSubmit={handleLogin}>
+    <Wrapper>
+      <animated.div style={animation}>
+        <Form onSubmit={handleLogin}>
+          <Title>Welcome Back</Title>
+          <Description>
+            Enter your email to receive the magic link
+          </Description>
           <div>
-            <input
-              className="inputField"
+            <Input
               type="email"
               placeholder="Your email"
               value={email}
@@ -38,12 +127,13 @@ export default function Auth() {
             />
           </div>
           <div>
-            <button className={"button block"} disabled={loading}>
-              {loading ? <span>Loading</span> : <span>Send magic link</span>}
-            </button>
+            <Button disabled={loading}>
+              {loading ? <span>Loading...</span> : <span>Send Magic Link</span>}
+            </Button>
           </div>
-        </form>
-      </div>
-    </div>
+        </Form>
+      </animated.div>
+    </Wrapper>
   );
 }
+
