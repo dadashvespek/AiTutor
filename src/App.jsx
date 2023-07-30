4972269e21bb040720485dd785464c320212b931
wilkieE <wilkieejiroghene@gmail.com>
2023-07-30 22:05:45 +0300

    added google login

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,59 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
-import Auth from "./Auth";
+// import Auth from "./Auth";
 import Account from "./Account";
 import NewSession from "./components/NewSession";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 function App() {
   const [session, setSession] = useState(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
+      console.log(data)
       setSession(session);
     });
 
     supabase.auth.onAuthStateChange((_event, session) => {
+      console.log(_event)
       setSession(session);
     });
+    console.log(session)
   }, []);
+
+  useEffect(() => { 
+    console.log(session)
+  }, [session]);
 
   return (
     <div>
       {!session ? (
-        <Auth />
-      ) : (
+        <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={["google", "facebook", "github"]}
+          />
+        </div>
+
+      </div>
+    ) : (
         // <Account key={session.user.id} session={session} />
-        <NewSession key={session.user.id} session={session} />
+        <div>
+          <NewSession key={session.user.id} session={session} />       
+          <button onClick={() => supabase.auth.signOut()}>Sign out</button>
+        </div>
       )}
     </div>
   );
