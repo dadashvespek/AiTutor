acd5069261d56433d699dee689c29da6130bf5af
wilkieE <wilkieejiroghene@gmail.com>
2023-08-01 13:18:47 +0300

    added navbar with signout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import logo from '../assets/favtutorlogo.png';
+import { supabase } from '../supabaseClient';
+
 
 const NavBar = ({session}) => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const userName = session.user.identities[0].identity_data.name;
     const userEmail = session.user.email;
 
+      
     useEffect(() => {
-      const pageClickEvent = (e) => {
-        setDropdownOpen(!dropdownOpen && e.target.closest('#user-menu-button'));
-      };
+        const pageClickEvent = (e) => {
+            if (!e.target.closest('#user-menu-button') && !e.target.closest('#user-dropdown')) {
+              setDropdownOpen(false);
+            }
+          };
   
       // If the item is active (ie open) then listen for clicks
       if (dropdownOpen) {
@@ -24,17 +30,16 @@ const NavBar = ({session}) => {
     return (
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center">
-              <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <a href="https://favtutor.com/" className="flex items-center">
+        <img src={logo} className="h-8 mr-3" alt="FavTutor Logo" />
           </a>
-          <div className="flex items-center md:order-2">
+          <div className="relative flex items-center md:order-2">
               <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom" onClick={() => setDropdownOpen(!dropdownOpen)}>
                 <span className="sr-only">Open user menu</span>
                 <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo" />
               </button>
               {dropdownOpen && (
-                <div className="z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+                <div className="z-50 absolute top-full right-0 mt-4 py-2 w-48 bg-white rounded-md shadow-xl dark:bg-gray-800" id="user-dropdown">
                   <div className="px-4 py-3">
                     <span className="block text-sm text-gray-900 dark:text-white">{userName}</span>
                     <span className="block text-sm text-gray-500 truncate dark:text-gray-400">{userEmail}</span>
@@ -59,6 +64,6 @@ const NavBar = ({session}) => {
         </div>
       </nav>
     );
-  }
+}
 
 export default NavBar;
