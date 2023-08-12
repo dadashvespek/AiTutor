db79f0dd6452d91ef1c04b04d8f4854055c86523
Danial Shafaei <56474459+dadashvespek@users.noreply.github.com>
2023-08-12 12:04:24 +0300

    as

--- src/components/CodeSandbox.jsx
+++ src/components/CodeSandbox.jsx
@@ -203,11 +203,11 @@ const CodeSandbox = ({ language, chatSession }) => {
         pauseOnHover
       />
 
-      {/* <div className="flex flex-row items-center justify-between px-5">
+      <div className="flex flex-row items-center justify-between px-5">
         <div className="px-4 py-2">
           <ThemeDropdown handleThemeChange={handleThemeChange} theme={theme} />
         </div>
-      </div> */}
+      </div>
       <div className="flex flex-row space-x-4 items-start px-4 py-4">
         <div className="flex flex-col w-full h-full justify-start items-end">
           {/* <h1 className="text-2xl font-bold text-white">
