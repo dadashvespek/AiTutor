20b266558fede4760d6a78f6dcddffaa370afa4a
Danial Shafaei <56474459+dadashvespek@users.noreply.github.com>
2023-08-13 23:25:10 +0300

    scroll bar

--- src/components/VoiceChat.jsx
+++ src/components/VoiceChat.jsx
@@ -25,6 +25,175 @@ const AccordionElement = ({ url, label, audioEls, id, isServerResponding }) => {
   ) 
 };
 
+const interviewTipsByLanguage = {
+  "javascript": [
+    "Familiarize yourself with the intricacies of the 'this' keyword and its context.",
+    "Understand the event loop and how JavaScript handles asynchrony, especially with Promises, async/await.",
+    "Be aware of JavaScript's prototype-based inheritance and how it differs from classical OOP.",
+    "Practice array manipulation methods like map, reduce, filter, and the spread/rest operator.",
+    "Brush up on ES6+ features like arrow functions, destructuring, and template literals.",
+    "Grasp the concept of closures and understand the benefits they bring, as well as common use-cases such as emulating private methods.",
+    "Understand memory management in JavaScript, especially how garbage collection works and how to avoid memory leaks.",
+    "Dive into the different ways of creating objects in JavaScript, such as using the `Object.create` method or the `new` keyword.",
+    "Be familiar with the different web APIs available in the browser environment, like the Document Object Model (DOM), Fetch API, and the History API.",
+    "Understand the concept of hoisting and how it affects the declaration and initialization of `var`, `let`, `const`, and functions.",
+    "Know the difference between shallow and deep copying of objects. Be familiar with techniques to achieve both.",
+    "Be prepared to explain how the `==` and `===` operators work, detailing the difference between type coercion and strict equality comparison.",
+    "Understand the various methods and patterns to handle exceptions, including `try`, `catch`, `finally`, and custom error handling techniques.",
+    "Dive into the intricacies of the `function` keyword, detailing first-class functions, higher-order functions, and functional programming concepts.",
+    "Get familiar with JavaScript modules: how to create, import, and bundle them using tools like Webpack or Rollup.",
+    "Understand the mechanics behind Service Workers and how they can be used to create Progressive Web Apps (PWAs).",
+    "Be well-versed with transpilers like Babel, and understand why and when you'd need them.",
+    "Get a grasp on JavaScript's concurrency model, especially how Web Workers and SharedArrayBuffer play a role in it.",
+    "Understand the performance implications of long-running JavaScript and how to use the browser's Performance API to measure and optimize code.",
+    "Dive deep into the different ways of handling state in applications, from simple variables to complex state management solutions like Redux.",
+    "Know the security concerns related to JavaScript, especially Cross-site Scripting (XSS) and how to prevent it.",
+    "Be familiar with AJAX, its working, and how it paved the way for modern asynchronous patterns.",
+    "Understand the different scopes in JavaScript: block scope, function scope, and global scope.",
+    "Know how to work with JavaScript in different environments, not just the browser but also Node.js.",
+    "Stay updated with the latest ECMA specifications and be ready to discuss upcoming and experimental features that aren't yet widely adopted."
+
+  ],
+  
+  "python": [
+    "Review list, dictionary, and set comprehensions for efficient inline transformations.",
+    "Ensure you understand Python's indentation-based scoping and how it affects control structures.",
+    "You can use 'zip' to combine keys and values into a dictionary or 'map' to apply a function to each element in a list.",
+    "Dive deep into decorators and how they can be used to modify function behaviors. Understand built-in decorators like @staticmethod, @classmethod, and @property.",
+    "Become comfortable with Python's exception handling using try, except, else, and finally blocks.",
+    "Understand and practice with Python's memory management, specifically referencing and garbage collection. Recognize the difference between shallow and deep copies using the copy module.",
+    "Familiarize yourself with Python's standard libraries like os, sys, datetime, and collections. For instance, practice navigating directories using os or using defaultdict from collections.",
+    "Review common Pythonic idioms and best practices. For example, using 'if not some_list' instead of 'if len(some_list) == 0'.",
+    "Brush up on Python's object-oriented programming concepts. Understand inheritance, polymorphism, encapsulation, and method overriding.",
+    "If the position involves web development, review frameworks like Flask or Django. Understand the basics of routing, templates, and ORM.",
+    "Study the differences between Python 2 and Python 3, especially if the company has legacy code. Understand changes in print statements, integer division, and the `input` function.",
+    "Be familiar with Python's module and package system. Know how to create, import, and use modules and packages, and the role of the `__init__.py` file.",
+    "Get comfortable with Python's list slicing syntax. Be able to extract sublists, reverse lists, and skip elements using slicing.",
+    "Brush up on lambda functions, and understand when to use them versus traditional function definitions.",
+    "Explore Python's asynchronous capabilities. Understand `asyncio`, `async/await` syntax, and when asynchronous programming can be beneficial.",
+    "Review common data structures and their Python implementations, such as stacks, queues, and linked lists.",
+    "Practice working with Python's file I/O, both with text and binary files. Understand reading and writing operations, and the difference between `read`, `readline`, and `readlines`.",
+    "Familiarize yourself with Python's `unittest` framework. Be able to write simple test cases and understand the importance of unit testing.",
+    "Understand Python's memory management and the difference between mutable and immutable types. Dive into how lists, dictionaries, and strings are stored and manipulated in memory.",
+    "Learn about Python's `itertools` and `functools` libraries. Understand the purpose and usage of functions like `chain`, `cycle`, `accumulate`, and `partial`.",
+    "Practice using Python's regular expression module, `re`. Understand pattern matching, searching, and replacing within strings.",
+    "Dive into type hinting introduced in Python 3.5 and its benefits. Be able to annotate functions and variables with their expected types.",
+    "Review Python's context managers and the `with` statement. Understand its importance in resource management, especially with file operations.",
+    "Understand the principles behind Python's generators and iterators. Be able to write and use generator functions using the `yield` statement.",
+    "Brush up on Python's `property` decorator and its use in creating getter, setter, and deleter methods for class attributes.",
+    "Know the differences between `is` and `==`. Understand Python's underlying object identity vs. value equality concepts.",
+    "Familiarize yourself with common Python design patterns such as Singleton, Factory, and Decorator. Understand their implementations and use-cases in Pythonic code.",
+    "Review performance optimization techniques in Python. Understand the implications of using local vs. global variables, list comprehensions vs. loops, and the benefits of JIT compilation with tools like PyPy.",
+    "Explore Python's metaclasses and understand their role in class creation and modification.",
+    "Understand the Global Interpreter Lock (GIL) in CPython. Know its implications for multi-threaded applications and alternatives for concurrency."
+
+
+  ],
+
+  "cpp": [
+    "Thoroughly understand memory management concepts, including the use of pointers, references, and memory leaks.",
+    "Get comfortable with C++'s Object-Oriented Programming, especially inheritance, polymorphism, and encapsulation.",
+    "Review the use and benefits of the Standard Template Library (STL), especially containers like vector, set, and map.",
+    "Familiarize yourself with template classes and functions in C++.",
+    "Understand the distinctions between C++11, C++14, and C++17 standards and their respective features.",
+    "Dive deep into memory management: Ensure you can demonstrate how to manually allocate and deallocate memory using 'new' and 'delete'.",
+    "Understand how smart pointers (unique_ptr, shared_ptr, weak_ptr) help in automatic memory management.",
+    "Be adept at C++'s Object-Oriented Programming: Beyond just the basics of inheritance, polymorphism, and encapsulation.",
+    "Master the Standard Template Library (STL): Show proficiency in various STL algorithms.",
+    "Be ready to demonstrate real-world use cases for containers like `vector`, `set`, `map`, `list`, and `unordered_map`.",
+    "Understand the differences and use cases for `emplace_back` vs `push_back`, and similar function pairs.",
+    "Templates in-depth: Not only should you familiarize yourself with template classes and functions ",
+    "Understand template metaprogramming, template specializations, and variadic templates.",
+    
+    "Differences in C++ standards: Understand and provide examples of features introduced in C++11, C++14, C++17, and even C++20 if possible.",
+    
+    "Concurrency in C++: Understand threads, mutexes, and how to write thread-safe code. Get familiar with the memory model and atomic operations.",
+    
+    "Error handling: Demonstrate the use of exceptions, try-catch blocks, and understand the nuances of exception specifications (`noexcept`).",
+    
+    "Optimization techniques: Understand inline functions, compiler optimization flags, and the importance of 'const' correctness.",
+    
+    "Design Patterns: While not C++ specific, having a grasp on common design patterns like Singleton, Factory, Observer, etc., and how they can be implemented in C++ can be beneficial.",
+    
+    "Move Semantics: Understand rvalue references, `std::move`, and the significance of move constructors and move assignment operators. Be prepared to explain how they can optimize performance.",
+    
+    "RAII Principle: Be familiar with the Resource Acquisition Is Initialization (RAII) principle in C++ and its importance in resource management.",
+    
+    "Lambda Expressions: Dive deep into lambda captures, mutable lambdas, and their typical use cases.",
+    
+    "C++ I/O: Understand nuances of the iostream library, especially manipulators and the differences between `cin`, `cout`, `cerr`, and `clog`.",
+    
+    "Compile and Linking Process: Understand the compilation and linking process in C++. Know about the One Definition Rule (ODR) and how linkage (internal, external, none) affects your code.",
+    
+    "Namespaces: Understand the significance of namespaces, the `using` directive vs. the `using` declaration, and potential pitfalls.",
+    
+    "C++ Casts: Be aware of the different types of casting in C++: `static_cast`, `dynamic_cast`, `const_cast`, and `reinterpret_cast`, and when each should be used.",
+    
+    "Modern C++ Features: Familiarize yourself with features from C++20 like coroutines, concepts, and ranges. It's beneficial to show that you're up-to-date with the latest standards.",
+    
+    "Platform-specific Development: Depending on the job, understanding cross-platform development, working with different compilers, or platform-specific quirks can be crucial.",
+    
+    "Code Organization: Understand the significance of header guards, `#pragma once`, and best practices for organizing large C++ projects.",
+    
+    "C++ Libraries and Frameworks: Familiarize yourself with popular C++ libraries and frameworks related to the job, like Boost, Qt, or POCO.",
+    
+    "Debugging: Show proficiency with debugging tools like gdb or Visual Studio's debugger. Understand how to interpret a stack trace and use debug symbols.",
+    
+    "Profile and Optimize: Know how to use tools like `valgrind` or `gprof` to profile application performance and pinpoint memory leaks.",
+    
+    "Modern Memory Practices: Understand the importance and usage of `alignas`, `alignof`, and the new operator overloads for alignment.",
+    
+    "Rule of Three/Five/Zero: Understand the rules associated with copy/move constructors and copy/move assignment operators, especially when custom memory management is involved.",
+    
+    "Iterators: Dive deep into the different types of iterators in STL, their properties, and use-cases: Input, Output, Forward, Bidirectional, and Random Access Iterators.",
+    
+    "Preprocessor Directives: Understand `#define`, `#ifdef`, `#ifndef`, and the difference between macros and inline functions.",
+    
+    "Type Traits and Type Utilities: Familiarize yourself with `type_traits` header, concepts like `is_same`, `remove_reference`, and how they can be used in template metaprogramming.",
+    
+    "Best Practices: Understand best practices like the use of `auto`, benefits of `constexpr`, when to use `inline`, and how to avoid code smells in C++.",
+    
+    "Backward Compatibility: Be aware of common pitfalls when dealing with legacy C++ code and how to safely modernize old code bases."
+ 
+ 
+  ],
+
+  "sql": [
+    "Ensure you're confident in writing JOIN operations across multiple tables, especially INNER JOIN, LEFT JOIN, and RIGHT JOIN.",
+    "Practice creating complex queries using GROUP BY, HAVING, and nested subqueries.",
+    "Understand the differences between primary and foreign keys and how they maintain database integrity.",
+    "Review data normalization concepts and the normal forms (1NF, 2NF, 3NF, etc.).",
+    "Get a grasp of transaction controls like COMMIT, ROLLBACK, and SAVEPOINT.",
+  ]
+};
+
+function InterviewBanner({ isServerResponding, language }) {
+  const [currentTipIndex, setCurrentTipIndex] = useState(0);
+
+  const interviewTips = interviewTipsByLanguage[language] || [];
+  
+  useEffect(() => {
+    if (isServerResponding) {
+      const interval = setInterval(() => {
+        const randomIndex = Math.floor(Math.random() * interviewTips.length);
+        setCurrentTipIndex(randomIndex);
+      }, 12000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isServerResponding, interviewTips.length]);
+
+  if (!isServerResponding || interviewTips.length === 0) return null;
+
+  return (
+    <div className={`banner fade-in scrolling-banner`}>
+      <span className="tip-title">{interviewTips[currentTipIndex]}</span>
+    </div>
+  );
+}
+
+
+
 
 const RecordingButton = ({ recording, handleStopRecording, handleStartRecording, isServerResponding }) => {
   return (
@@ -53,6 +222,7 @@ export default function VoiceRecorder({chatSession, session}) {
     formData.append('type', chatSession.type);
     formData.append('difficulty', chatSession.difficulty);
     formData.append('language', chatSession.language.value);
+    console.log(`language: ${chatSession.language.value}`)
     formData.append('userName', userName);
 
     const response = await fetch(`${import.meta.env.VITE_SERVER_URL}audio`, {
@@ -133,8 +303,8 @@ export default function VoiceRecorder({chatSession, session}) {
   const originalAudioEls = useRef([]);
   const serverAudioEls = useRef([]);
   
- return (
-    <Box className="voice-recorder w-1/2 h-[86vh] py-4 mt-4" display="flex" flexDirection="column">
+  return (
+    <Box className="voice-recorder w-1/2 h-[80vh] py-4 mt-4" display="flex" flexDirection="column">
       <Box flexGrow={1} className="accordion-window w-[40vw]">
         {originalAudioURLs.map((url, index) => (
           <Box 
@@ -164,7 +334,9 @@ export default function VoiceRecorder({chatSession, session}) {
           {recording ? "Recording..." : "Click Mic to start recording"}
         </Typography>
       </Box>
+      <InterviewBanner isServerResponding={isServerResponding} language={chatSession.language.value} />
     </Box>
 );
+
 ;
 }
