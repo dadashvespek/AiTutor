65199ed1a4672b063fc482a99e06b1f6529b9974
Danial Shafaei <56474459+dadashvespek@users.noreply.github.com>
2023-09-12 22:53:26 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ai-interviewer/',
   plugins: [react()],
+  base: '/ai-interviewer/',
+  build: {
+    outDir: 'dist/ai-interviewer'
+  }
 })
